911a1966898736a2f503deb52af9e0604bd7e459
julianomenzen <juliano.menzen@gmail.com>
2018-04-09 20:21:57 -0300

    inclusao do modelo de instagram

---
+++ HappyPlace/HappyPlace/models/instragram.py
@@ -0,0 +1,14 @@
+class instragram(object):
+    """description of class"""
+    latitude = 0.0
+    longitude = 0.0
+    cnpj = ""
+    tag = ""
+
+    def __init__(self, latitude, longitude, cnpj, tag):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.cnpj = cnpj
+        self.tag = tag
+
+
--- HappyPlace/HappyPlace/services/hospedagemService.py
+++ HappyPlace/HappyPlace/services/hospedagemService.py
@@ -52,19 +52,15 @@ class hospedagemService(object):
                             x.append("")
        
                     hospesagemLinha = hospedagem(x[0],x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12], x[13], x[14], x[15], x[16], x[17], x[18], x[19], x[20], x[21], x[22], x[23], x[24], x[25], x[26], x[27], x[28], x[29])
-                    t = Thread(target=self.salvarHospedagem,args=[service_correios, hospesagemLinha, i, content, tokens])
-                    t.start()
-                    threads.append(t)
                     if (not repositorio.validaExistePorCNPJ(hospesagemLinha.cnpj)):
-                        t = Thread(target=self.salvarHospedagem,args=[service_correios, hospesagemLinha, i, content, repositorio])
+                        t = Thread(target=self.salvarHospedagem,args=[service_correios, hospesagemLinha, i, content, repositorio, tokens])
                         t.start()
                         threads.append(t)
                     #self.salvarHospedagem(service_correios, hospesagemLinha, i, content)
                     
                 i = i + 1
 
-    def salvarHospedagem(self, service_correios, hospesagemLinha, i, content, tokens):
-    def salvarHospedagem(self, service_correios, hospesagemLinha, i, content, repositorio):
+    def salvarHospedagem(self, service_correios, hospesagemLinha, i, content, repositorio, tokens, ):
         #Será que vai dar desempenho? Me pareceu que sim!
         self.buscarCidadeLatLong(service_correios, hospesagemLinha, tokens)
         repositorio.inserir(hospesagemLinha)
